df867062e48ef5fa07e04f87de5c9ee1dced2342
Naushad Warsi <warsinak@gmail.com>
2018-05-27 08:06:12 +0000

    Create client.py

---
+++ TCP/python/fileTransfer/client.py
@@ -0,0 +1,30 @@
+import sys
+import socket
+
+sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#define server address:
+srvr_add=('localhost',22002)
+
+print>>sys.stderr,'connecting to %s port %s' % srvr_add
+
+sock.connect(srvr_add)
+f=open("tosend.txt","r")
+
+try:
+    #tosend ='this is the echo message'
+    tosend=f.read(50)
+    print>>sys.stderr,'sending "%s"' %tosend
+    sock.sendall(tosend)
+
+    bits_recv=0
+    bits_expected = len(tosend)
+
+    while bits_recv < bits_expected:
+        data=sock.recv(20)
+        bits_recv+=len(data)
+        print>>sys.stderr,'received: "%s"' %data
+
+finally:
+    print>>sys.stderr,'closing socket'
+    sock.close()
